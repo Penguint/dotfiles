62e7291a536651a948a896ab73c333cb192d76fb
Penguint <tjpyytqz@outlook.com>
2020-09-13 01:46:19 +0800

    Add .cf-framework

---
+++ .cf-template/acm-framework.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define sz(v) (int)v.size()
+#define debug(var) cout << #var << ": " << var << endl;
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int main() {
+}
